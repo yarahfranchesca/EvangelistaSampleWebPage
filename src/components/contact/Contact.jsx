f34810adbd40ec414c3236de8697965b1f4bf3cf
Yarah Franchesca <yarahfranchescae@gmail.com>
2023-08-09 19:40:40 +0800

    initial commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,6 +10,10 @@ const Contact = () => {
                 <div className="contact__info">
                     <h3 className="contact__title">Where to reach me out:</h3>
                     <p className="contact__details">Let's work together! Send me an email ♡</p>
+                    <p className="contact__details">Email: yarahfranchescae@gmail.com</p>
+                    <p className="contact__details">Contact #: 09452058422</p>
+
+
                 </div>
 
         <form action="" className="contact__form">
